52bec3b1a396e91c49d2a652d381d1d001f4284d
Christopher Olano <olanochristopher@gmail.com>
2021-01-20 23:46:37 +0800

    broken our homepage into components, add styling menu-item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,42 +1,13 @@
 import React from "react";
 
+import Directory from "../../components/directory/directory.component.jsx";
+
 import "./homepage.styles.scss";
 
 const Homepage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">HATS</h1>
-                        <subtitle className="subtitle">SHOW NOW</subtitle>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Jackets</h1>
-                        <subtitle className="subtitle">SHOW NOW</subtitle>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Sneakers</h1>
-                        <subtitle className="subtitle">SHOW NOW</subtitle>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Womens</h1>
-                        <subtitle className="subtitle">SHOW NOW</subtitle>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Mens</h1>
-                        <subtitle className="subtitle">SHOW NOW</subtitle>
-                    </div>
-                </div>
-            </div>
+            <Directory />
         </div>
     );
 };
