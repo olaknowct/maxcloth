172431b8cd74782b28c74c0b126202a4a6d689e4
Christopher Olano <olanochristopher@gmail.com>
2021-01-24 02:52:16 +0800

    update our menu-items to navigate withRouter HIGH ORDER COMPONENT HOC

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Route from "react-router-dom";
 import "./App.css";
 
 import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+    return <div>hello</div>;
+};
 
 function App() {
     return (
