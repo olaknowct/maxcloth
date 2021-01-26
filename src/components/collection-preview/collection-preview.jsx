634e6ae5d28b2e9e73be811a5a40b84f6b210fd7
Christopher Olano <olanochristopher@gmail.com>
2021-01-27 00:51:57 +0800

    update collection preview and add collection item component

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
+import CollectionItem from "../collection-item/collection-item.component";
 import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => {
     return (
         <div className="collection-preview">
-            <h1 className="title">test</h1>
+            <h1 className="title">{title}</h1>
             <div className="preview">
                 {items
                     .filter((item, idx) => idx < 4)
-                    .map((item) => {
-                        return <div key={item.id}>{item.name}</div>;
+                    .map(({ id, ...otherItemProps }) => {
+                        return <CollectionItem key={id} {...otherItemProps} />;
                     })}
             </div>
         </div>
