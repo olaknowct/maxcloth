a16999c9c309ffb53e3c142e1b95dc941732f663
Christopher Olano <olanochristopher@gmail.com>
2021-01-31 23:28:41 +0800

    signin submit function

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -17,10 +17,16 @@ class SignIn extends React.Component {
         };
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
 
-        this.setState({ email: "", password: "" });
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: "", password: "" });
+        } catch (err) {
+            console.log(err);
+        }
     };
 
     handleChange = (event) => {
@@ -39,7 +45,7 @@ class SignIn extends React.Component {
                     <FormInput
                         name="email"
                         value={this.state.email}
-                        handleChange={this.state.handleChange}
+                        handleChange={this.handleChange}
                         required
                         label="email"
                         type="email"
@@ -47,8 +53,8 @@ class SignIn extends React.Component {
                     <FormInput
                         name="password"
                         type="password"
-                        value={this.state.email}
-                        handleChange={this.state.handleChange}
+                        value={this.state.password}
+                        handleChange={this.handleChange}
                         label="password"
                         required
                     />
