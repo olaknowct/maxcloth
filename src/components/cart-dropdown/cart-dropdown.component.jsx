6287d1956fe15ea3e90fd4c4156265ed01cbb57b
Christopher Olano <olanochristopher@gmail.com>
2021-02-07 23:56:23 +0800

    updating cart dropdown/icon component with reselect | for performance

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import "./cart-dropdown.styles.scss";
 
 const CartDropDown = ({ cartItems }) => {
@@ -19,9 +20,9 @@ const CartDropDown = ({ cartItems }) => {
     );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => {
+const mapStateToProps = (state) => {
     return {
-        cartItems,
+        cartItems: selectCartItems(),
     };
 };
 
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,17 @@
+import { createSelector } from "reselect";
+
+// input selector
+const selectCart = (state) => state.cart;
+
+// output selector
+// 1st argument : collection of input selector, function that gets the output of the input
+export const selectCartItems = createSelector(
+    [selectCart],
+    (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    // .map((cartItem) => cartItem.quantity)
+    [selectCartItems],
+    (cartItems) => cartItems.reduce((acc, cur) => acc + cur.quantity, 0)
+);
