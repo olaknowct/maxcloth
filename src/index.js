945f33a58516a177d63365eef749db7f0b888b18
Christopher Olano <olanochristopher@gmail.com>
2021-02-13 01:19:31 +0800

    Add redux-persist and bring it to the table

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import App from "./App";
 
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import { persistStore } from "redux-persist";
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
             <React.StrictMode>
-                <App />
+                <PersistGate persistor={persistor}>
+                    <App />
+                </PersistGate>
             </React.StrictMode>
         </BrowserRouter>
     </Provider>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,24 @@
 // actual base object that represents all of the state of our application
 // actual vcode the combines all other state together
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+
+// actual local storage, you can also use session storage
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+// persist config = json object that represent configuration we want
+const persistConfig = {
+    key: "root", //at what point isnide of our reducer object do we want to store  evernything
+    storage,
+    whitelist: ["cart"], // containing string name of any reducer we want to store, persist only what is need, cart
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
