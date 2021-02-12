d646a67ff96cedc7b0c5e4721dec117c43e5078d
Christopher Olano <olanochristopher@gmail.com>
2021-02-12 22:57:00 +0800

    add/deduct cart quantity added

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -7,9 +7,31 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         return cartItems.map((cartItem) =>
             cartItem.id === cartItemToAdd.id
                 ? { ...cartItem, quantity: cartItem.quantity + 1 }
-                : cartItem
+                : { ...cartItem }
         );
     }
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    // get the item to remove from the cart
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    );
+
+    // if quantity is 1, return new array excluding the item to remove
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(
+            (cartItem) => cartItem.id !== cartItemToRemove.id
+        );
+    }
+
+    return cartItems.map((cartItem) =>
+        cartItem.id === cartItemToRemove.id
+            ? // new object
+              { ...cartItem, quantity: cartItem.quantity - 1 }
+            : // object?
+              { ...cartItem }
+    );
 };
