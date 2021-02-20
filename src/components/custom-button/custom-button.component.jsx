41759f2dfb9e3cce493057173df1645bfcd24daa
Christopher Olano <olanochristopher@gmail.com>
2021-02-20 13:48:26 +0800

    fix styling custom button component

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 
 // import "./custom-button.styles.scss";
-import { CustomButtonContainer } from "./custom-button.styles";
+// import { CustomButtonContainer } from "./custom-button.styles";
 
-const CustomButton = ({ children, ...Props }) => {
-    return <CustomButtonContainer {...Props}>{children}</CustomButtonContainer>;
-};
+// const CustomButton = ({ children, ...Props }) => {
+//     return <CustomButtonContainer {...Props}>{children}</CustomButtonContainer>;
+// };
 
 // scss
-// import "./custom-button.styles.scss";
+import "./custom-button.styles.scss";
 
-// const CustomButton = ({
-//     children,
-//     isGoogleSignin,
-//     inverted,
-//     ...otherProps
-// }) => {
-//     return (
-//         <button
-//             className={`${inverted ? "inverted" : ""} ${
-//                 isGoogleSignin ? "google-sign-in" : ""
-//             } custom-button`}
-//             {...otherProps}
-//         >
-//             {children}
-//         </button>
-//     );
-// };
+const CustomButton = ({
+    children,
+    isGoogleSignin,
+    inverted,
+    ...otherProps
+}) => {
+    return (
+        <button
+            className={`${inverted ? "inverted" : ""} ${
+                isGoogleSignin ? "google-sign-in" : ""
+            } custom-button`}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+};
 export default CustomButton;
